fb2c246044dffa4c9ab04be60b3a7ecb5ddb59ec
Myros <fromzeroinc@gmail.com>
2025-01-26 18:56:44 +0200

    Added Vector2 extension or rotation

---
+++ Extensions/VectorExtensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Core.Scripts.Extensions
+{
+    public static class VectorExtensions
+    {
+        public static Vector2 RotatedByAngle(this Vector2 vec, float angleDegrees)
+        {
+            float angleRad = angleDegrees * Mathf.Deg2Rad;
+            Vector2 newVec =
+                new Vector2(
+                   vec.x * Mathf.Cos(angleRad) - vec.y * Mathf.Sin(angleRad),
+                   vec.x * Mathf.Sin(angleRad) + vec.y * Mathf.Cos(angleRad));
+            return newVec;
+        }
+    }
+}
